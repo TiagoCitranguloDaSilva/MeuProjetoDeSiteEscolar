e1a8e14a0bf4f59e52728944d42435ca3990d6d4
Tiago Citrangulo da Silva <130724423+TiagoCitranguloDaSilva@users.noreply.github.com>
2023-05-16 18:55:45 -0300

    Só uma olhada

--- php/aulas.php
+++ php/aulas.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="../js/script.js"></script>
     <link rel="shortcut icon" href="../fav/fav_cde.png" type="image/x-icon">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/aula.css">
     <title>Aulas</title>
 </head>
 <body>
